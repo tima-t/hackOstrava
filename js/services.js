96f1d7466da15e5d6a58d810612d22b617989ec3
tima_t <tito.marchev@gmail.com>
2016-10-28 01:03:49 +0300

    review trans

--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 var services = angular.module('starter.services',[])
 
-.service('LoginService',function(api,$q,$http,$httpParamSerializerJQLike) {
+.service('LoginService',function(api,$q,$http) {
     return {
 
         loginUser: function(name, pw) {
@@ -25,6 +25,41 @@ var services = angular.module('starter.services',[])
 									window.localStorage.setItem("username", name);
 									window.localStorage.setItem("password", pw);
 									deferred.resolve(resp.data);
+								}
+								else{
+									console.log(error);
+									console.log("error");
+								deferred.reject(resp.data);
+								}
+							},
+							function(resp){
+								deferred.reject(resp.data);
+							});
+            return promise;
+        }
+    }
+})
+.service('TransactionsService',function(api,$q,$http) {
+    return {
+
+        getLatest: function() {
+            var deferred = $q.defer();
+            var promise = deferred.promise;
+						var req =
+						{
+ 							method: 'Get',
+ 							url: api+'/transactions',
+							params: {
+      				token : window.localStorage.getItem('token')
+						},
+							headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+						}
+						$http(req).then(
+							function(resp){
+								console.log(resp.data.response);
+								if(resp.data.response && resp.data.response.status == 'OK' ){
+									console.log("response");
+									deferred.resolve(resp.data.response);
 								}
 								else{
 									console.log(error);
